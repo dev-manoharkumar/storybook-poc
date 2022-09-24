31a0eb06603546bae6cebb6874574c03fe285100
Manohar Kumar <manohar@netomi.com>
2022-09-24 20:40:52 +0530

    change task background to normal

--- src/components/Task.js
+++ src/components/Task.js
@@ -31,7 +31,6 @@ export default function Task({task: {id, title, state}, onArchiveTask, onPinTask
           readOnly={true}
           name="title"
           placeholder="Input title"
-          style={{background: 'red'}}
         />
       </label>
       
